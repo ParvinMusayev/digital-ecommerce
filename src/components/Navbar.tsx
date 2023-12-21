c16acdb5e0260f93ba65accfdcb7579b2080ccd7
ParvinMusayev <parvinmusayev1@gmail.com>
2023-12-22 00:42:04 +0400

    created NavItem

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
@@ -8,7 +11,15 @@ const Navbar = () => {
         <MaxWidthWrapper>
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
-                <div className="ml-4 flex lg:ml-0"></div>
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
+                </Link>
+              </div>
+
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
