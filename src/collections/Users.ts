4e2dede9cc9e84e93ec0f57826791ba68a527609
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-05 15:39:50 +0400

    error fixed

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,44 +2,50 @@ import { CollectionConfig } from "payload/types";
 
 
 export const Users: CollectionConfig = {
-    slug: 'users',
-    auth: true,
-    access: {
-        read: () => true,
-        create: () => true,
-        
-      },
-    fields: [
-        // {
-        //     name: 'products',
-        //     label: 'Products',
-        //     admin: {
-        //       condition: () => false,
-        //     },
-        //     type: 'relationship',
-        //     relationTo: 'products',
-        //     hasMany: true,
-        //   },
-        //   {
-        //     name: 'product_files',
-        //     label: 'Product files',
-        //     admin: {
-        //       condition: () => false,
-        //     },
-        //     type: 'relationship',
-        //     relationTo: 'product_files',
-        //     hasMany: true,
-        //   },
-          {
-            name: 'role',
-            defaultValue: 'user',
-            required: true,
-      
-            type: 'select',
-            options: [
-              { label: 'Admin', value: 'admin' },
-              { label: 'User', value: 'user' },
-            ],
-          },
-    ]
+  slug: 'users',
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<p>hello pls verify</p>`
+      }
+    }
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+
+  },
+  fields: [
+    // {
+    //     name: 'products',
+    //     label: 'Products',
+    //     admin: {
+    //       condition: () => false,
+    //     },
+    //     type: 'relationship',
+    //     relationTo: 'products',
+    //     hasMany: true,
+    //   },
+    //   {
+    //     name: 'product_files',
+    //     label: 'Product files',
+    //     admin: {
+    //       condition: () => false,
+    //     },
+    //     type: 'relationship',
+    //     relationTo: 'product_files',
+    //     hasMany: true,
+    //   },
+    {
+      name: 'role',
+      defaultValue: 'user',
+      required: true,
+
+      type: 'select',
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'User', value: 'user' },
+      ],
+    },
+  ]
 }
