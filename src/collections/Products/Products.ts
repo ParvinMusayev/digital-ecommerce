e5a150f0305860138fa5fff4e5813eaa2142f58e
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-09 23:36:55 +0400

    feat: Expand Products collection with additional fields

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,4 +1,4 @@
-import { PRODUCT_CATEGORIES } from "@/config";
+import { PRODUCT_CATEGORIES } from "../../config";
 import { CollectionConfig } from "payload/types";
 
 export const Products: CollectionConfig = {
@@ -53,31 +53,75 @@ export const Products: CollectionConfig = {
             required: true,
             relationTo: 'product_files',
             hasMany: false,
-          },
-          {
+        },
+        {
             name: 'approvedForSale',
             label: 'Product Status',
             type: 'select',
             defaultValue: 'pending',
             access: {
-              create: ({ req }) => req.user.role === 'admin',
-              read: ({ req }) => req.user.role === 'admin',
-              update: ({ req }) => req.user.role === 'admin',
+                create: ({ req }) => req.user.role === 'admin',
+                read: ({ req }) => req.user.role === 'admin',
+                update: ({ req }) => req.user.role === 'admin',
             },
             options: [
-              {
-                label: 'Pending verification',
-                value: 'pending',
-              },
-              {
-                label: 'Approved',
-                value: 'approved',
-              },
-              {
-                label: 'Denied',
-                value: 'denied',
-              },
+                {
+                    label: 'Pending verification',
+                    value: 'pending',
+                },
+                {
+                    label: 'Approved',
+                    value: 'approved',
+                },
+                {
+                    label: 'Denied',
+                    value: 'denied',
+                },
             ],
-          },
+        },
+        {
+            name: 'priceId',
+            access: {
+                create: () => false,
+                read: () => false,
+                update: () => false,
+            },
+            type: 'text',
+            admin: {
+                hidden: true,
+            },
+        },
+        {
+            name: 'stripeId',
+            access: {
+                create: () => false,
+                read: () => false,
+                update: () => false,
+            },
+            type: 'text',
+            admin: {
+                hidden: true,
+            },
+        },
+        {
+            name: 'images',
+            type: 'array',
+            label: 'Product images',
+            minRows: 1,
+            maxRows: 4,
+            required: true,
+            labels: {
+                singular: 'Image',
+                plural: 'Images',
+            },
+            fields: [
+                {
+                    name: 'image',
+                    type: 'upload',
+                    relationTo: 'media',
+                    required: true,
+                },
+            ],
+        },
     ]
 }
