f23b19bdeaabf9cc09289de504cdcb001a043d20
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-14 13:17:18 +0400

    feat: Add CartItem component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { ShoppingCart } from "lucide-react";
 import {
   Sheet,
@@ -14,51 +15,50 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import Image from "next/image";
 import { useCart } from "@/hooks/use-cart";
+import { ScrollArea } from "./ui/scroll-area";
+import CartItem from "./CartItem";
 import { useEffect, useState } from "react";
 
 const Cart = () => {
-  const { items } = useCart()
-  const itemCount = items.length
+  const { items } = useCart();
+  const itemCount = items.length;
 
-  const [isMounted, setIsMounted] = useState<boolean>(false)
+  const [isMounted, setIsMounted] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsMounted(true)
-  }, [])
+    setIsMounted(true);
+  }, []);
 
   const cartTotal = items.reduce(
     (total, { product }) => total + product.price,
     0
-  )
-
+  );
 
   const fee = 1;
+
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
         <ShoppingCart
           aria-hidden="true"
-          className="w-6 h-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {isMounted ? itemCount : 0}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
             <div className="flex w-full flex-col pr-6">
-           
+              <ScrollArea>
                 {items.map(({ product }) => (
-                  <CartItem
-                    product={product}
-                    key={product.id}
-                  />
+                  <CartItem product={product} key={product.id} />
                 ))}
-              
+              </ScrollArea>
             </div>
             <div className="space-y-4 pr-6">
               <Separator />
@@ -103,8 +103,7 @@ const Cart = () => {
                 alt="empty shopping cart hippo"
               />
             </div>
-            <div className="text-xl font-semibold ">Your cart is empty</div>
-
+            <div className="text-xl font-semibold">Your cart is empty</div>
             <SheetTrigger asChild>
               <Link
                 href="/products"
